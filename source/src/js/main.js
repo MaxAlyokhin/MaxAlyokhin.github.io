288ad8cb1303fb7288c2ec2ab7e506395264b3c3
Max Alyokhin <55048182+MaxAlyokhin@users.noreply.github.com>
2022-03-14 23:05:52 +0300

    actimind test update

--- source/src/js/main.js
+++
@@ -1,7 +0,0 @@
-import humburgerInit from './header.js'
-import tableInit from './table.js'
-
-window.addEventListener('DOMContentLoaded', () => {
-  humburgerInit() // Инициализируем мобильное меню
-  tableInit() // Инициализируем datatables.js
-})

80fbaaadc9b14542e0c6269c43d08ea535313584
Max Alyokhin <55048182+MaxAlyokhin@users.noreply.github.com>
2024-10-27 20:21:59 +0300

    Cenix test

---
+++ assets/default-DinqOba5.js
@@ -0,0 +1,1 @@
+import{d as o,c as s,w as e,V as n,r,o as c,a,b as _}from"./index-BgKsRGnY.js";const d=o({__name:"default",setup(p){return(l,u)=>{const t=r("router-view");return c(),s(n,null,{default:e(()=>[a(_,{class:"mx-5"},{default:e(()=>[a(t)]),_:1})]),_:1})}}});export{d as default};
